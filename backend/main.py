dcd262202ec1befcc647e1762cc6960782d17792
merlinaCabrera <merlinacabreramc@gmail.com>
2026-07-30 15:06:32 -0300

    dominio

--- backend/main.py
+++ backend/main.py
@@ -60,7 +60,9 @@ origins = [
     "http://127.0.0.1:5173",
     "https://club-atletico-api.onrender.com", 
     "https://car-frontend-dusky.vercel.app",
-    "https://car-frontend-git-main-merlina-s-projects.vercel.app"
+    "https://car-frontend-git-main-merlina-s-projects.vercel.app",
+    "https://clubatleticoroberts.com",
+    "https://www.clubatleticoroberts.com",
 ]
 
 app.add_middleware(
